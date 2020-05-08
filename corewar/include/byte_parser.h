18b94be0583f56e4e7d0a4195aea24e7f85b4703
truchman <dorian.truchon@epitech.eu>
2020-05-08 11:58:12 +0200

    Commit to checkout

---
+++ corewar/include/byte_parser.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_corewar_2019
+** File description:
+** byte_parser.h
+*/
+
+#ifndef CPE_COREWAR_2019_BYTE_PARSER_H
+#define CPE_COREWAR_2019_BYTE_PARSER_H
+
+typedef struct byte_parser_s
+{
+    int p1 : 2;
+    int p2 : 2;
+    int p3 : 2;
+    int p4 : 2;
+} byte_parser_t;
+
+#endif //CPE_COREWAR_2019_BYTE_PARSER_H
--- corewar/src/instructions/instruction_add.c
+++ corewar/src/instructions/instruction_add.c
@@ -7,12 +7,15 @@
 
 #include "vm.h"
 #include "champion.h"
+#include "byte_parser.h"
 
 int instruction_add(vm_t *vm, champion_t *champion)
 {
+    byte_parser_t params = {0};
+
     if (!vm || !champion)
         return 84;
-
+    ++champion->pc;
 
 
     return 0;
