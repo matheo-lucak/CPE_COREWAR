5fd084f9758b8758249b5529f5508143a4d1f36c
matheo.lucak <matheo.lucak@epitech.eu>
2020-05-15 18:51:45 +0200

    [FIX] lldi had the same behavior as ld and lld. Changed for having the same behavior has lldi
    [ADD] Tests instructions ldi/lldi/lld

--- corewar/src/instructions/instruction_lldi.c
+++ corewar/src/instructions/instruction_lldi.c
@@ -16,20 +16,21 @@ int instruction_lldi(vm_t *vm, champion_t *champion)
     instr_params_t params = {0};
     size_t values[2] = {0};
     size_t tmp_pc = 0;
+    size_t index = 0;
 
     if (!vm || !champion)
         return 84;
     tmp_pc = champion->pc + 1;
     tmp_pc %= MEM_SIZE;
     if (get_instruction_params(vm->memory, (int *)&tmp_pc,
-            &params, i_lldi) == 84)
+            &params, i_ldi) == 84)
         return 84;
     if (get_ops_params_values(&params, vm->memory, champion, values) == 84)
         return 84;
-    tmp_pc += values[0] + values[1];
-    tmp_pc %= MEM_SIZE;
-    if (my_memcpy(&values[0], champion->registers +
-                            REG_SIZE * params.values[1], REG_SIZE) == 84)
+    index = champion->pc + (values[0] + values[1]);
+    index %= MEM_SIZE;
+    if (my_memcpy(&vm->memory[index], champion->registers +
+        REG_SIZE * params.values[2], REG_SIZE) == 84)
         return 84;
     champion->pc = tmp_pc;
     champion->carry = true;
