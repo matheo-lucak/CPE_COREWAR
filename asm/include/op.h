205b42f680526fc60ee72aabb3cd2fd7d9ca3193
matheo.lucak <matheo.lucak@epitech.eu>
2020-05-16 16:30:31 +0200

    [UPDATE] Cleaning repo (usless root/src & root/include)

--- asm/include/op.h
+++ asm/include/op.h
@@ -22,26 +22,18 @@
 #define NAME_CMD_STRING ".name"
 #define COMMENT_CMD_STRING ".comment"
 
-/*
-** regs
-*/
-
-#define REG_NUMBER 16 /* r1 <--> rx */
-
-/*
-**
-*/
+#define REG_NUMBER 16
 
 typedef char args_type_t;
 
-#define T_REG 1 /* register */
-#define T_DIR 2 /* direct  (ld  #1 r1  put 1 into r1) */
-#define T_IND 4 // indirect always relative
-                //( ld 1 r1 put what's in the address (1+pc)
-                //into r1 (4 bytes ))
-#define T_LAB 8 /* LABEL */
+#define T_REG 1
+#define T_DIR 2
+#define T_IND 4
 
-struct op_s {
+#define T_LAB 8
+
+struct op_s
+{
     char *mnemonique;
     char nbr_args;
     args_type_t type[MAX_ARGS_NUMBER];
@@ -52,28 +44,16 @@ struct op_s {
 
 typedef struct op_s op_t;
 
-/*
-** size (in bytes)
-*/
-
 #define IND_SIZE 2
 #define DIR_SIZE 4
 #define REG_SIZE DIR_SIZE
 
-/*
-** op_tab
-*/
-
 extern op_t op_tab[];
-
-/*
-** header
-*/
 
 #define PROG_NAME_LENGTH 128
 #define COMMENT_LENGTH 2048
 
-#define COREWAR_EXEC_MAGIC 0xea83f3        /* why not */
+#define COREWAR_EXEC_MAGIC 0xea83f3
 
 struct header_s {
     int magic;
@@ -84,11 +64,7 @@ struct header_s {
 
 typedef struct header_s header_t;
 
-/*
-** live
-*/
-
-#define CYCLE_TO_DIE 1536    /* number of cycle before being declared dead */
+#define CYCLE_TO_DIE 1536
 #define CYCLE_DELTA 5
 #define NBR_LIVE 40
 
