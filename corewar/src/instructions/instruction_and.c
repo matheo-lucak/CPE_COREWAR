ae72b85ac38d70656205062a8afbe00e3f1cb192
matheo.lucak <matheo.lucak@epitech.eu>
2020-05-17 21:02:41 +0200

    [FIX NORM] instruction add >80 clmns
    [FIX] Dump bonus really prints memory

--- corewar/src/instructions/instruction_and.c
+++ corewar/src/instructions/instruction_and.c
@@ -22,7 +22,7 @@ int instruction_and(vm_t *vm, champion_t *champion)
         return 84;
     tmp_pc = increment_pc(champion->pc, 1);
     if (get_instruction_params(vm->memory, &tmp_pc, &params, i_and) == 84 ||
-            get_logicals_params_values(&params, vm->memory, champion, values) == 84)
+        get_logicals_params_values(&params, vm->memory, champion, values) == 84)
         return 84;
     result = (size_t)values[0] & (size_t)values[1];
     if (my_memcpy(&result,
