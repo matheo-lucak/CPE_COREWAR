750a7de9f4a3784d35b69040a4984402e438a86f
matheo.lucak <matheo.lucak@epitech.eu>
2020-05-17 11:20:10 +0200

    [UPDATE] Memory pavé now decrease depending cycle2die

--- corewar/src/vm/update_memory_3d.c
+++ corewar/src/vm/update_memory_3d.c
@@ -12,16 +12,22 @@
 #include "vector_engine.h"
 #include "vm.h"
 
-static int update_color_height(char memory, char memory_dup,
-        memory_3d_t *memory_3d, int color)
+static int update_color_height(char memory_diff,
+        memory_3d_t *memory_3d, int color, int cycle_to_die)
 {
+    float decrease_rate = 0;
+
     if (!memory_3d)
         return 84;
-    if (memory != memory_dup) {
+    if (cycle_to_die > 0)
+        decrease_rate = 1 / (float)cycle_to_die;
+    else
+        decrease_rate = 0.1;
+    if (memory_diff) {
         memory_3d->color = color;
         memory_3d->height = MAX_TILE_HEIGHT;
     } else if (memory_3d->height > 0)
-        memory_3d->height -= 0.02;
+        memory_3d->height -= decrease_rate;
     return 0;
 }
 
@@ -37,8 +43,8 @@ int update_memory_3d(vm_t *vm, map_formatter_t *map, champion_t *champion)
     for (y = 0; y < map->map_settings.size.y; y += 1) {
         for (x = 0; x < map->map_settings.size.x; x += 1) {
             i = D2TOD1(x, y, map->map_settings.size.x);
-            if (update_color_height(vm->memory[i], vm->memory_dup[i],
-                &map->map_3d[y][x], color) == 84)
+            if (update_color_height(vm->memory[i] - vm->memory_dup[i],
+                &map->map_3d[y][x], color, vm->cycle_to_die) == 84)
             return 84;
             map->map_2d[y][x] = project_iso_point(
                 VEC3F(x, y, map->map_3d[y][x].height),
