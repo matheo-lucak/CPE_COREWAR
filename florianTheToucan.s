2f63faf6ebc06d4ff02721cf791d5f12b878e6e0
matheo.lucak <matheo.lucak@epitech.eu>
2020-05-20 14:47:20 +0200

    [FLORIAN WANTS TO LIVE]

--- florianTheToucan.s
+++ florianTheToucan.s
@@ -1,25 +1,28 @@
 	.name "Florian"
 	.comment "It's florian and he is a Toucan ;)"
 
-ld %0 r1
-ld %1 r2
+ld %33 r1
+lld %16777216 r2
 ld %33 r3
 ld %0 r4
 ld %8 r5
-ld %9 r6
-ld %-5 r7
+lld %218102528 r6
+lld %-5 r7
+ld %0 r16
 
-add r1 r1 r1
-fork %:florian_has_child_1
+add r16 r16 r16
+
+florian_nursery:
+    fork %:florian_wants_to_expand
+    fork %:florian_has_child_1
 
 florian_wants_to_expand:
     live %33
     add r4 r5 r4
-    sti r2 %24 r4
-    sti r3 %22 r4
-    sti r6 %17 r4
-    sti r7 %13 r4
-    add r1 r1 r1
+    sti r2 %18 r4
+    sti r3 %13 r4
+    sti r6 %11 r4
+    add r16 r16 r16
     zjmp %:florian_wants_to_expand
 
 florian_has_child_1:
